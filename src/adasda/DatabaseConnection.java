f9e5d6aaded0fa713d6f3437088301582b0b1f44
potma <potma@LAPTOP-PLK16SGC>
2019-06-30 21:30:05 +0200

    #5

---
+++ src/adasda/DatabaseConnection.java
@@ -0,0 +1,32 @@
+package adasda;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+
+import javax.swing.JOptionPane;
+
+public class DatabaseConnection {
+	
+	final static String JDBC_DRIVER="com.mysql.jdbc.Driver";
+	final static String DB_URL="jdbc:mysql://localhost:3306/test";
+	
+	final static String USER="root";
+	final static String PAS="";
+	
+	public static Connection connection()
+	{
+		try {
+			
+			Class.forName(JDBC_DRIVER);
+			Connection conn = DriverManager.getConnection(DB_URL, USER, PAS);
+			return conn;
+			
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e);
+			return null;
+		}
+		
+	}
+
+}
